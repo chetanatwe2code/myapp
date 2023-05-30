fadf08453b01864e15f6c465fec0662c4b9576d3
chetan <chtnbarod@gmail.com>
2023-05-30 15:45:28 +0530

    new update

--- src/css/AuthStyles.js
+++ src/css/AuthStyles.js
@@ -1,4 +1,7 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet , Dimensions } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 export const styles = StyleSheet.create({
   container: {
